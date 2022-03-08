424f20411b2971ea999cec24ffefdcedf9d3ce89
Helton Bustos <heltonsmith@hotmail.com>
2022-03-07 21:38:23 -0300

    Ejemplo SharedPreference 2 - Login

--- app/src/main/java/com/heltonbustos/ejemplosharedpreference01/MainActivity.java
+++ app/src/main/java/com/heltonbustos/ejemplosharedpreference01/MainActivity.java
@@ -7,38 +7,48 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText txtNombre, txtCorreo;
+    EditText txtCorreo, txtClave;
     Button btnGuardar;
+    CheckBox chkRecordar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNombre = findViewById(R.id.txtNombre);
         txtCorreo = findViewById(R.id.txtCorreo);
+        txtClave = findViewById(R.id.txtClave);
         btnGuardar = findViewById(R.id.btnGuardar);
+        chkRecordar = findViewById(R.id.chkRecordar);
 
         SharedPreferences preferences = getSharedPreferences("almacenamiento_sp", Context.MODE_PRIVATE);
-        txtNombre.setText(preferences.getString("sp_nombre", ""));
         txtCorreo.setText(preferences.getString("sp_correo", ""));
+        txtClave.setText(preferences.getString("sp_clave", ""));
 
         btnGuardar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String nombre = txtNombre.getText().toString();
-                String correo = txtCorreo.getText().toString();
+                if (chkRecordar.isChecked()){
+                    String correo = txtCorreo.getText().toString();
+                    String clave = txtClave.getText().toString();
 
-                SharedPreferences preferences = getSharedPreferences("almacenamiento_sp", Context.MODE_PRIVATE);
-                SharedPreferences.Editor editor = preferences.edit();
+                    SharedPreferences preferences = getSharedPreferences("almacenamiento_sp", Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = preferences.edit();
 
-                editor.putString("sp_nombre", nombre);
-                editor.putString("sp_correo", correo);
-                editor.commit();
+                    editor.putString("sp_correo", correo);
+                    editor.putString("sp_clave", clave);
+                    editor.commit();
+                    Toast.makeText(MainActivity.this, "Cuenta almacenada", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Cuenta no almacenada", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
